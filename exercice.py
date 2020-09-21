e8ab750c735200191a65dac648afe358a2777f43
anto252 <antoinecc@hotmail.ca>
2020-09-21 14:47:07 -0400

    Working

--- exercice.py
+++ exercice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+import math
 from typing import List
 
 
@@ -19,15 +19,22 @@ def use_prefixes() -> List[str]:
 
 
 def prime_integer_summation() -> int:
-    for num in range(0, 100 + 1):
-        if num > 1:
-            for i in range(2, num):
-                if (num % i) == 0:
-                    break
-            else:
-                print(num)
+    primes = []
 
-    return num
+    i = 2
+    while len(primes) < 100:
+        is_prime = True
+
+        for divider in range(2, int(math.sqrt(i)) + 1):
+            if i % divider == 0:
+                is_prime = False
+                
+        if is_prime:
+            primes.append(i)
+
+        i += 1
+    
+    return sum(primes)
 
 
 def factorial(number: int) -> int:
